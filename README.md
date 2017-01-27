6b1699cbc5363fb3125b50b59ec1edf4651c68d5
jucet <jucet1@gmail.com>
2017-01-27 16:30:41 +0000

    accented characters replaced

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Semana
 Semana.com news scraper 
+
+#References and Links
+
+http://stackoverflow.com/questions/6159118/using-java-to-pull-data-from-a-webpage
+
+http://www.theasciicode.com.ar/ascii-printable-characters/tilde-swung-dash-ascii-code-126.html
+
+http://www.utf8-chartable.de/
--- src/OpenArticle.java
+++ src/OpenArticle.java
@@ -7,7 +7,8 @@ import java.net.URLConnection;
 import java.util.Scanner;
 import tools.Parse;
 
-// http://stackoverflow.com/questions/6159118/using-java-to-pull-data-from-a-webpage
+
+// view-source:http://www.dinero.com/economia/articulo/echavarria-llega-a-su-primera-junta-directiva-del-banco-de-la-republica/241407
 public class OpenArticle {
 
 	private static String pageContent="";
@@ -15,6 +16,7 @@ public class OpenArticle {
 	public static void main(String[] args) throws IOException {
 
 		String prn="";
+		String replace="";
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("http://www.semana.com");
@@ -40,29 +42,36 @@ public class OpenArticle {
 		
 			prn = Parse.GetContentBetweenAutomation(pageContent,"articleBody","<p","</p>",true,1);
 			
+			// insert a loop to remove all html tags by using
+			// GetContentBetween to copy the string to remove starting from < and finishing in >
+			// then remove using replaceAll()
+			
+			replace = Parse.GetContentBetweenAutomation(prn, "", "<", ">", true, 2);
+			
+			prn = prn.replaceAll("&ntilde;", "\u00F1");
+			prn = prn.replaceAll("&aacute;", "\u00E1");
+			prn = prn.replaceAll("&eacute;", "\u00E9");
+			prn = prn.replaceAll("&iacute;", "\u00ED");
+			prn = prn.replaceAll("&oacute;", "\u00F3");
+			prn = prn.replaceAll("&uacute;", "\u00FA");
+			
 			prn = prn.replaceAll("&nbsp;", " ");
-			prn = prn.replaceAll("&", "");
-			prn = prn.replaceAll("ntilde;", "ñ");
-			prn = prn.replaceAll("acute;", "");
-			prn = prn.replaceAll("\n", "");
+
 			prn = prn.replaceAll("<p>", "");
 			prn = prn.replaceAll("<em>", "");
 			prn = prn.replaceAll("</em>", "");
 			prn = prn.replaceAll("<br/>", "\n");
-			prn = prn.replaceAll("<", "");
-			prn = prn.replaceAll(">", "");
+			
+			prn = prn.replaceAll("\n", "");
 			prn = prn.replaceAll("  ", "");
 			
 			// insert returns
 			// split prn in 4 and concatenate the returns
 			
 			 // String temp = prn.substring(0, 100);
-			 
-			
-			
-			
 				
 			System.out.println(prn);
+			System.out.println(replace);
 		
 	}	
 
